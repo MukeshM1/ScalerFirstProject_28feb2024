31ccf46b903b97c51878c7039231ede4f71da2ef
MukeshM1 <mukeshmanjunath199@gmail.com>
2024-03-10 23:49:11 +0530

    Completed FakeStore API

--- src/main/java/dev/mukeshm1/scalerfirstproject_28feb2024/Controllers/ProductController.java
+++ src/main/java/dev/mukeshm1/scalerfirstproject_28feb2024/Controllers/ProductController.java
@@ -2,20 +2,29 @@ package dev.mukeshm1.scalerfirstproject_28feb2024.Controllers;
 
 import dev.mukeshm1.scalerfirstproject_28feb2024.Models.Product;
 import dev.mukeshm1.scalerfirstproject_28feb2024.Services.ProductService;
+import dev.mukeshm1.scalerfirstproject_28feb2024.dtos.CreateProductRequestDto;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.CacheRequest;
+import java.util.List;
 
 @RestController
 public class ProductController {
-
     private ProductService productService;
+    private RestTemplate restTemplate;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, RestTemplate restTemplate) {
         this.productService = productService;
+        this.restTemplate = restTemplate;
     }
 
     @PostMapping("/products")
-    public void creatProduct() {
-
+    public Product creatProduct(@RequestBody CreateProductRequestDto request) {
+        return productService.createProduct(
+                request.getTitle(), request.getPrice(), request.getCatergory(),
+                request.getDiscription(), request.getImage()
+        );
     }
 
     @GetMapping("/products/{id}")
@@ -23,17 +32,31 @@ public class ProductController {
         return productService.getSingleProduct(productId);
     }
 
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable("id")Long productId) {
+        productService.deleteProduct(productId);
+    }
+
     @GetMapping("/products")
-    public void getAllProducts() {
-
+    public List<Product> getAllProducts() {
+        return productService.getallproducts();
+    }
+    @GetMapping("/proucts/category/{category}")
+    public List<Product> getProductsByCategory(@PathVariable("category") String category) {
+        return productService.getProductsByCategory(category);
     }
 
-    public void updateProduct() {
-
+    @PatchMapping("/products/{id}")
+    public Product updateProduct(@PathVariable("id") Long productId, @RequestBody CreateProductRequestDto request) {
+        return productService.updateProduct(
+                productId, request.getTitle(), request.getPrice(), request.getCatergory(),
+                request.getDiscription(), request.getImage()
+        );
     }
 
-    @DeleteMapping("/products")
-    public void deleteProduct() {
-
+    @GetMapping("/products/categories")
+    public List<String> getAllCategories() {
+        return productService.getAllCategories();
     }
+
 }
